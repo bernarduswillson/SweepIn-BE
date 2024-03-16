5ced40c44a48fa7d535b28b21da13b604f3d0ab7
Ahmad Nadil <13521024@std.stei.itb.ac.id>
2024-03-16 15:15:56 +0700

    fix(swagger): attendace docs

--- src/attendance/attendance.swagger.ts
+++ src/attendance/attendance.swagger.ts
@@ -3,44 +3,138 @@
  * tags :
  *  - name: Attendance
  *    description: Attendance related endpoints
- * /api/attendance:
- *  get:
- *      summary: Get all attendance
- *      description: Get all attendance
- *      tags: [Attendance]
- *      parameters:
- *       - in: header
- *         name: x-api-key
+ * /attendance:
+ *   get:
+ *     summary: Retrieve a list of attendances
+ *     description: Retrieve a list of attendances for a specific user within a date range. The attendances are returned in pages.
+ *     tags: [Attendance]
+ *     parameters:
+ *       - in: query
+ *         name: user_id
+ *         schema:
+ *           type: string
+ *           example: 65f54e3e74e075636481087b
+ *         description: The user id
+ *       - in: query
+ *         name: start_date
+ *         schema:
+ *           type: string
+ *           example: 2021-08-01T00:00:00.000Z
+ *         description: The start date in ISO 8601 format
+ *       - in: query
+ *         name: end_date
+ *         schema:
+ *           type: string
+ *           example: 2024-08-31T23:59:59.999Z
+ *         description: The end date in ISO 8601 format
+ *       - in: query
+ *         name: page
  *         required: true
  *         schema:
- *          type: string
- *          example: secretapikey
- *      responses:
- *          '200':
- *              description: A successful response
- *          '500':
- *              description: Internal server error
- * /api/attendance/{attendanceId}:
- *  get:
- *      summary: Get attendance details by Id
- *      description: Get attendance details by Id
- *      tags: [Attendance]
- *      parameteres:
- *       - in: header
- *         name: x-api-key
+ *           type: string
+ *           example: 1
+ *         description: The current page
+ *       - in: query
+ *         name: per_page
  *         required: true
  *         schema:
- *          type: string
- *          example: secretapikey
+ *           type: string
+ *           example: 10
+ *         description: The number of data in a page
+ *     responses:
+ *       200:
+ *         description: A list of attendances
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Get all attendance successful
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/Attendance'
+ *
+ * /attendance/{attendanceId}:
+ *   get:
+ *     summary: Retrieve attendance details
+ *     description: Retrieve specific attendance details by attendance id.
+ *     tags: [Attendance]
+ *     parameters:
  *       - in: path
  *         name: attendanceId
- *         required: true
- *         description : attendanceId
  *         schema:
- *          type: string
- *      responses:
- *          '200':
- *              description: A successful response
- *          '500':
- *              description: Internal server error 
+ *           type: string
+ *           example: 65f54e5774e07563648108cd
+ *         required: true
+ *         description: The attendance id
+ *     responses:
+ *       200:
+ *         description: Attendance details
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Get attendance details successful
+ *                 data:
+ *                   $ref: '#/components/schemas/Attendance'
+ *
+ * components:
+ *   schemas:
+ *     Log:
+ *       type: object
+ *       required:
+ *         - id
+ *         - date
+ *         - image
+ *         - latitude
+ *         - longitude
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the log
+ *         date:
+ *           type: string
+ *           format: date-time
+ *           description: The date of the log
+ *         image:
+ *           type: string
+ *           description: The image associated with the log
+ *         latitude:
+ *           type: number
+ *           format: float
+ *           description: The latitude where the log was created
+ *         longitude:
+ *           type: number
+ *           format: float
+ *           description: The longitude where the log was created
+ *
+ *     Attendance:
+ *       type: object
+ *       required:
+ *         - id
+ *         - date
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the attendance
+ *         date:
+ *           type: string
+ *           format: date-time
+ *           description: The date of the attendance
+ *         startLog:
+ *           type: array
+ *           items:
+ *             $ref: '#/components/schemas/Log'
+ *           description: The start log of the attendance
+ *         endLog:
+ *           type: array
+ *           items:
+ *             $ref: '#/components/schemas/Log'
+ *           description: The end log of the attendance
  */
