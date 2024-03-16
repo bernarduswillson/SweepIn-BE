f886f14598deab320c97642442b596d427d5bb46
Ahmad Nadil <13521024@std.stei.itb.ac.id>
2024-03-16 18:09:53 +0700

    refactor(server): create server as a function

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import dotenv from "dotenv"
+
+import { createServer } from "./utils/server"
+
+dotenv.config()
+
+const app = createServer()
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running on port ${PORT}`)
+})
