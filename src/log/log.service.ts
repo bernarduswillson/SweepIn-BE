0bd775e5d694a4998d2d5c1d981689180a4aae76
Ahmad Nadil <13521024@std.stei.itb.ac.id>
2024-05-03 00:06:17 +0700

    refactor(end): start and end create update
    - Log now have 2 endpoint, start an end
    - Log will check first if the start/end is already created, if it hasn't been created, it will create, else it will update.

--- src/log/log.service.ts
+++ src/log/log.service.ts
@@ -1,5 +1,13 @@
 import { generateAttendance } from '../attendance/attendance.service'
-import { createLog, createLogImage } from './log.repository'
+import {
+  createStartLog,
+  updateLog,
+  createEndLog,
+  createLogImage,
+  getTodayStartLog,
+  getTodayEndLog,
+  removeLogImage
+} from './log.repository'
 import { findOneAttendance } from '../attendance/attendance.repository'
 import qrcode from 'qrcode'
 import sharp from 'sharp'
@@ -7,53 +15,47 @@ import { createCanvas } from 'canvas'
 import fs from 'fs'
 import { getUserById } from '../auth/auth.repository'
 import { NotFoundError } from '../class/Error'
+import { Log } from '@prisma/client'
 
-/**
- * Submit log
- *
- * @description Create log, if corresponding attendance does not exist, then create new attendance. else, bind attendanceId to the created log
- * @returns attendanceId, logId
- */
-const submitLog = async (
-  attendanceId: string,
-  userId: string,
-  date: string,
-  image: Express.Multer.File[],
-  latitude: string,
-  longitude: string
-) => {
-  const userExists = await getUserById(parseInt(userId))
-
-  if (!userExists) {
-    throw new NotFoundError('User not found')
-  }
-
-  const _ = await generateAndOverlayImage(
-    'http://www.sweepin.itb.ac.id/log/138',
-    image,
-    'Example text'
-  )
-
-  return attendanceId
-    ? await submitEndLog(attendanceId, date, image, latitude, longitude)
-    : await submitStartLog(userId, date, image, latitude, longitude)
-}
-
-const submitStartLog = async (
+const startLog = async (
   userId: string,
   date: string,
   images: Express.Multer.File[],
   latitude: string,
   longitude: string
 ) => {
-  const attendanceId = await generateAttendance(parseInt(userId))
+  const startLogExists = await getTodayStartLog(parseInt(userId))
 
-  const { id } = await createLog(
-    date,
-    parseFloat(latitude),
-    parseFloat(longitude),
-    attendanceId,
-    undefined
+  return startLogExists
+    ? editStartLog(
+        startLogExists,
+        date,
+        parseFloat(latitude),
+        parseFloat(longitude),
+        images
+      )
+    : submitStartLog(
+        date,
+        parseFloat(latitude),
+        parseFloat(longitude),
+        await generateAttendance(parseInt(userId)),
+        images
+      )
+}
+
+const submitStartLog = async (
+  date: string,
+  latitude: number,
+  longitude: number,
+  attendanceId: number,
+  images: Express.Multer.File[]
+) => {
+  const { id } = await createStartLog(date, latitude, longitude, attendanceId)
+
+  const _ = await generateAndOverlayImage(
+    'http://www.sweepin.itb.ac.id/log/138',
+    images,
+    'Example text'
   )
 
   for (const image of images) {
@@ -61,6 +63,49 @@ const submitStartLog = async (
   }
 
   return { attendanceId, id }
+}
+
+const editStartLog = async (
+  startLog: Log,
+  date: string,
+  latitude: number,
+  longitude: number,
+  images: Express.Multer.File[]
+) => {
+  await updateLog(startLog.id, date, latitude, longitude)
+  await removeLogImage(startLog.id)
+
+  const _ = await generateAndOverlayImage(
+    'http://www.sweepin.itb.ac.id/log/138',
+    images,
+    'Example text'
+  )
+
+  for (const image of images) {
+    await createLogImage(startLog.id, image.path)
+  }
+
+  return { attendanceId: startLog.attendanceStartId, id: startLog.id }
+}
+
+const endLog = async (
+  attendanceId: string,
+  date: string,
+  images: Express.Multer.File[],
+  latitude: string,
+  longitude: string
+) => {
+  const endLogExists = await getTodayEndLog(parseInt(attendanceId))
+
+  return endLogExists
+    ? editEndLog(
+        endLogExists,
+        date,
+        parseFloat(latitude),
+        parseFloat(longitude),
+        images
+      )
+    : submitEndLog(attendanceId, date, images, latitude, longitude)
 }
 
 const submitEndLog = async (
@@ -73,18 +118,23 @@ const submitEndLog = async (
   const attendanceExists = await findOneAttendance(parseInt(attendanceId))
 
   if (!attendanceExists) {
-    throw new Error('Attendance does not exist')
+    throw new NotFoundError('Attendance does not exist')
   }
 
   if (attendanceExists.endLog.length > 0) {
     throw new Error('Attendance already ended')
   }
 
-  const { id } = await createLog(
+  const _ = await generateAndOverlayImage(
+    'http://www.sweepin.itb.ac.id/log/138',
+    images,
+    'Example text'
+  )
+
+  const { id } = await createEndLog(
     date,
     parseFloat(latitude),
     parseFloat(longitude),
-    undefined,
     parseInt(attendanceId)
   )
 
@@ -95,72 +145,106 @@ const submitEndLog = async (
   return { attendanceId: parseInt(attendanceId), id }
 }
 
+const editEndLog = async (
+  endLog: Log,
+  date: string,
+  latitude: number,
+  longitude: number,
+  images: Express.Multer.File[]
+) => {
+  await updateLog(endLog.id, date, latitude, longitude)
+  await removeLogImage(endLog.id)
+
+  const _ = await generateAndOverlayImage(
+    'http://www.sweepin.itb.ac.id/log/138',
+    images,
+    'Example text'
+  )
+
+  for (const image of images) {
+    await createLogImage(endLog.id, image.path)
+  }
+
+  return { attendanceId: endLog.attendanceEndId, id: endLog.id }
+}
+
 const generateAndOverlayImage = async (
   qrText: string,
   images: Express.Multer.File[],
   text: string
 ) => {
-    const text1: string = 'Lorem Ipsum sir Dolor'
-    const text2: string = 'Example Text'
-    const date: string = '19 April 2024'
-    const time: string = '23:59'
-    const langLong: string = `123.45 432.31`
-    const id: string = 'id : 69420'
-    const web: string = 'www.sweepin.itb.ac.id'
-    const base = fs.readFileSync('./storage/basewatermark.png')
-    const logo = fs.readFileSync('./storage/sweepin-logo.png')
+  const text1: string = 'Lorem Ipsum sir Dolor'
+  const text2: string = 'Example Text'
+  const date: string = '19 April 2024'
+  const time: string = '23:59'
+  const langLong: string = `123.45 432.31`
+  const id: string = 'id : 69420'
+  const web: string = 'www.sweepin.itb.ac.id'
+  const base = fs.readFileSync('./storage/basewatermark.png')
+  const logo = fs.readFileSync('./storage/sweepin-logo.png')
 
-    const qrCode = await (qrcode.toBuffer as any)(qrText, {
-        width: 315,
-        height: 315
-    })
+  const qrCode = await (qrcode.toBuffer as any)(qrText, {
+    width: 315,
+    height: 315
+  })
 
-    const baseBuffer = await sharp(base)
-        .composite ([{ input: qrCode, top: 5, left: 5 }])
-        .toBuffer()
+  const baseBuffer = await sharp(base)
+    .composite([{ input: qrCode, top: 5, left: 5 }])
+    .toBuffer()
 
-    const baseBuffer2 = await sharp(baseBuffer)
-        .composite ([{input: logo, top: 82, left: 586}])
-        .toBuffer()
-    
+  const baseBuffer2 = await sharp(baseBuffer)
+    .composite([{ input: logo, top: 82, left: 586 }])
+    .toBuffer()
 
-    const canvas = createCanvas(470, 93)
-    const context = canvas.getContext('2d')
+  const canvas = createCanvas(470, 93)
+  const context = canvas.getContext('2d')
 
-    context.clearRect(0,0,470,93)
+  context.clearRect(0, 0, 470, 93)
 
-    context.font = '20px Arial'
-    context.fillStyle = '#00000'
-    context.textAlign = 'start'
-    context.fillText(id, 10, 30)
-    context.fillText(web, 10, 55)
+  context.font = '20px Arial'
+  context.fillStyle = '#00000'
+  context.textAlign = 'start'
+  context.fillText(id, 10, 30)
+  context.fillText(web, 10, 55)
 
-    const baseBuffer3 = await sharp(baseBuffer2)
-        .composite([{ input: Buffer.from(canvas.toDataURL().split(',')[1], 'base64'), top: 217, left: 320 }])
-        .toBuffer()
-    
-    const canvas2 = createCanvas(249, 211)
-    const context2 = canvas2.getContext('2d')
+  const baseBuffer3 = await sharp(baseBuffer2)
+    .composite([
+      {
+        input: Buffer.from(canvas.toDataURL().split(',')[1], 'base64'),
+        top: 217,
+        left: 320
+      }
+    ])
+    .toBuffer()
 
-    context2.clearRect(0,0,249,211)
+  const canvas2 = createCanvas(249, 211)
+  const context2 = canvas2.getContext('2d')
 
-    context2.font = '20px Arial'
-    context2.fillStyle = '#00000'
-    context2.textAlign = 'start'
-    context2.fillText(date, 10, 30)
-    context2.fillText(time, 10, 55)
-    context2.fillText(langLong, 10, 80)
-    context2.fillText(text1, 10, 105)
-    context2.fillText(text2, 10, 130)
+  context2.clearRect(0, 0, 249, 211)
 
-    const baseBuffer4 = await sharp(baseBuffer3)
-        .composite([{ input: Buffer.from(canvas2.toDataURL().split(',')[1], 'base64'), top: 5, left: 320 }])
-        .toBuffer()
+  context2.font = '20px Arial'
+  context2.fillStyle = '#00000'
+  context2.textAlign = 'start'
+  context2.fillText(date, 10, 30)
+  context2.fillText(time, 10, 55)
+  context2.fillText(langLong, 10, 80)
+  context2.fillText(text1, 10, 105)
+  context2.fillText(text2, 10, 130)
+
+  const baseBuffer4 = await sharp(baseBuffer3)
+    .composite([
+      {
+        input: Buffer.from(canvas2.toDataURL().split(',')[1], 'base64'),
+        top: 5,
+        left: 320
+      }
+    ])
+    .toBuffer()
 
   const processedImages = await Promise.all(
     images.map(async (image) => {
       let imageBuffer = fs.readFileSync(image.path)
-      
+
       const imgData = await sharp(imageBuffer).metadata()
       let imgWidth = imgData.width!
       let imgHeight = imgData.height!
@@ -176,10 +260,9 @@ const generateAndOverlayImage = async (
       const watermarkedImage = await sharp(imageBuffer)
         .composite([{ input: baseBuffer4, gravity: 'southwest' }])
         .toBuffer()
-      
+
       const filename = `./storage/attendances/${image.filename.split('.')[0]}.${image.filename.split('.')[1]}`
       fs.unlinkSync(image.path)
-
 
       fs.writeFileSync(filename, watermarkedImage)
       return filename
@@ -189,4 +272,4 @@ const generateAndOverlayImage = async (
   return processedImages
 }
 
-export { submitLog }
+export { startLog, endLog }
